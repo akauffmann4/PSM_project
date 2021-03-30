222ad6adea7f9e70a1838d4b7213a38f34f03183
Vatra Dimitrie <vatradimitrie@gmail.com>
2021-03-30 15:33:31 +0200

    diwoaiodaioaewioadew

--- src/main/scala/align.scala
+++ src/main/scala/align.scala
@@ -20,24 +20,24 @@ object align {
 
       val dsGroup = ui.createGroup("datasets")
 
-      val meshFiles = new java.io.File("C:/Users/audek/psm/datasets/challenge-data/challengedata/full-femurs/meshes/").listFiles
+      val meshFiles = new java.io.File("datasets/challenge-data/challengedata/full-femurs/meshes/").listFiles.take(10)
       val (meshes, meshViews) = meshFiles.map(meshFile => {
         val mesh = MeshIO.readMesh(meshFile).get
         val meshView = ui.show(dsGroup, mesh, "mesh")
         (mesh, meshView) // return a tuple of the mesh and the associated view
       }) .unzip // take the tuples apart, to get a sequence of meshes and one of meshViews
 
-      val reference = new java.io.File("C:/Users/audek/psm/data/femur.stl")
+      val reference = new java.io.File("data/femur.stl")
       val toAlign : Array[TriangleMesh[_3D]] = meshes
 
       val refLandmarks: Seq[Landmark[_3D]] =
-        LandmarkIO.readLandmarksJson3D(new java.io.File("C:/Users/audek/psm/data/femur.json")).get
+        LandmarkIO.readLandmarksJson3D(new java.io.File("data/femur.json")).get
 
       /*val landmarks: Array[Seq[Landmark[_3D]]] =
-        LandmarkIO.readLandmarksJson3D(new java.io.File("C:/Users/audek/psm/datasets/challenge-data/challengedata/full-femurs/landmarks/").listFiles).get
+        LandmarkIO.readLandmarksJson3D(new java.io.File("datasets/challenge-data/challengedata/full-femurs/landmarks/").listFiles).get
       */
-      var landmarksFiles = new java.io.File("C:/Users/audek/psm/datasets/challenge-data/challengedata/full-femurs/landmarks/").listFiles
-      //var refLandmarks = new java.io.File("C:/Users/audek/psm/data/femur.json")
+      var landmarksFiles = new java.io.File("datasets/challenge-data/challengedata/full-femurs/landmarks/").listFiles.take(10)
+      //var refLandmarks = new java.io.File("data/femur.json")
 
       val landmarks: Array[Seq[Landmark[_3D]]]= landmarksFiles.map { file =>
         LandmarkIO.readLandmarksJson3D(file).get
@@ -49,9 +49,9 @@ object align {
       val alignedMeshes = toAlign.map { mesh =>
         //val landmarks = pointIds.map{id => Landmark("L_"+id, mesh.pointSet.point(PointId(id)))}
         val rigidTrans = LandmarkRegistration.rigid3DLandmarkRegistration(landmarks(i), refLandmarks, center = Point(0,0,0))
-        mesh.transform(rigidTrans)
+        val meshAligned = mesh.transform(rigidTrans)
         i+=1
-        val meshView = ui.show(dsGroup2, mesh, "meshAligned")
+        val meshView = ui.show(dsGroup2, meshAligned, "meshAligned")
       }
       /*val dc = DataCollection.fromTriangleMesh3DSequence(reference, alignedMeshes)
       val modelFromDataCollection = PointDistributionModel.createUsingPCA(dc)
